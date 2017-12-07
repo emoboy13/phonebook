578b8665c20422cc30aebcb5c0ca9ea3a26d498e
function86437 <function86437@gmail.com>
2017-12-07 23:50:58 +0800

    Decrease the structure of entry Move __PHONE_BOOK_ENTRY content to __INFO, except lastname.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,8 +6,9 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 // #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+
+
+typedef struct __INFO {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +18,12 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+};
+
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+	struct __INFO *info;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
